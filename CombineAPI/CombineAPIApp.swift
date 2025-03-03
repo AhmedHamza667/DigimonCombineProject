a818055a77ac0dd52808e4ae2a3b269aa1b2ff5d
AhmedHamza667 <hamzaa667788@gmail.com>
2025-03-03 13:12:18 -0500

    Initial Commit

---
+++ CombineAPI/CombineAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAPIApp.swift
+//  CombineAPI
+//
+//  Created by Ahmed Hamza on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
